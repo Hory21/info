ac58f4873e4ce0fc16d7ae2588abfe9269b7baf5
Hory21 <oancea_horatiu@yahoo.com>
2014-10-01 15:49:25 +0300

    added input check fp lab 1 hw

--- fp/lab1_hw.py
+++ fp/lab1_hw.py
@@ -1,5 +1,9 @@
+import sys
 year = int(input("year:"))
 nr = int(input("day number:"))
+if nr > 366 or (nr > 365 and year % 4 != 0):
+	print('Invalid day number')
+	sys.exit()
 month = 0;
 if year % 4:
 	daysInMonth = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
