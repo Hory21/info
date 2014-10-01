da2b2ba179a5a8f0ab34c761bd18f8a795fab349
Hori21 <oancea_horatiu@yahoo.com>
2014-10-01 10:09:54 +0300

    fp lab 1 hw

---
+++ fp/lab1_hw.py
@@ -0,0 +1,16 @@
+year = int(input("year:"))
+nr = int(input("day number:"))
+month = 0;
+if year % 4:
+	daysInMonth = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+else:
+	daysInMonth = (31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)	
+i = 0
+aux = daysInMonth[0]
+for i in range(12):
+	if aux >= nr:
+		month = i + 1
+		day = daysInMonth[i] - (abs(aux - nr))
+		break
+	aux += daysInMonth[i + 1]
+print("data:", year, '.', month, '.', day, sep = '')
