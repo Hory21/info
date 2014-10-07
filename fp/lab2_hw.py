b2cd061e47e8e0ee17d3cd225a1065ea8e3b8f94
Hory21 <oancea_horatiu@yahoo.com>
2014-10-07 13:25:21 +0300

    progress on lab 2 hw

--- fp/lab2_hw.py
+++ fp/lab2_hw.py
@@ -1,9 +1,20 @@
 #cerintele 2, 14
 
 def readList():
-	n = int(input("Numarul de elemente din lista: "))
-	lis = []
-	print("Elementele listei (cate unul pe linie):")
-	for i in range(n):
-		lis.append(int(input()))
-	return lis
+    n = int(input("Numarul de elemente din lista: "))
+    lis = []
+    print("Elementele listei (cate unul pe linie):")
+    for i in range(n):
+        lis.append(int(input()))
+    return lis
+
+def checkProp1(lst):
+    auxlst = []
+    auxlst.append(lst[0])
+    for element in lst:
+        if not (element in auxlst):
+            auxlst.append(element)
+    if len(auxlst) <= 3:
+        return True
+    else:
+        return False
