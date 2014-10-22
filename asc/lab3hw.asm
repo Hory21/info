273dca37b15360a070b98504d308950f7dbd322c
Hory21 <oancea_horatiu@yahoo.com>
2014-10-22 16:57:51 +0300

    lab3hw asc

---
+++ asc/lab3hw.asm
@@ -0,0 +1,31 @@
+; enunt: (h+128)-(yy+m+d)
+
+assume cs: code, ds: data
+data segment
+	m db 4
+	h dw 185
+	yy dw 95
+	d db 1
+data ends
+code segment
+start:
+	mov ax, data
+	mov ds, ax
+
+	mov dx, h
+	add dx, 128 ; dx = h + 128 (acum, in dx se afla rezultatul primei paranteze)
+	mov dx, rez ; rez = h + 128
+	mov al, m
+	mov ah, 0
+	mov dx, ax ; dx = m
+	mov al, d
+	cbw
+	add dx, ax ; dx = m + d
+	add dx, yy ; dx = m + d + yy (acum, in dx se afla rezultatul celei de-a
+	           ; doua paranteze)
+	sub rez, dx ; rez = (h + 128) - (m + d + yy)
+
+	mov ax, 4C00h
+	int 21h
+code ends
+end start
